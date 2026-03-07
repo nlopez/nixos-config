03b44fd4b879c0b08ae21f9a7cc6623a39cbe5a9
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-03-07 04:31:15 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+	  ./configuration.nix
+	];
+      };
+    };
+  };
+}
+
